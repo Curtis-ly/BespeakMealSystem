187bb4537754222aed4a71f80831c66ae3b04719
Curtis-ly <liuyang8903@163.com>
2014-07-10 09:47:34 +0800

    创建工程
    创建Windows、IOS、Android工程

---
+++ IOS/BespeakMealSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BespeakMealSystem
+//
+//  Created by ly on 14-7-4.
+//  Copyright (c) 2014年 Galaxywind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
